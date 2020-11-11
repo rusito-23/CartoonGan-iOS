d7f95bb7cbb3441b82c8b8e83044eabeaf9d3cbe
Igor Andruskiewitsch <i.andruskiewitsch23@gmail.com>
2020-11-11 19:31:59 -0300

    : Initial Commit

---
+++ CartoonGan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CartoonGan
+//
+//  Created by Igor on 11/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
